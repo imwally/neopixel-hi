6675e64ec8f75c0482d46a2c8d8353f85209b6ad
Wally Jones <wally@wallyjones.com>
2019-03-20 23:25:06 -0400

    Add the beginnings of some scrolling bits

--- include/gol.h
+++ include/gol.h
@@ -2,3 +2,4 @@
 #define WIDTH 8
 
 void printGrid(int grid[HEIGHT][WIDTH]);
+void shiftGrid(int grid[HEIGHT][WIDTH]);
--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,14 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 
+int grid[HEIGHT][WIDTH] = {
+        {0,1,0,1,0,1,0,0},
+        {0,1,0,1,0,0,0,0},
+        {0,1,1,1,0,1,0,0},
+        {0,1,0,1,0,1,0,0},
+};
+
+
 void setup() {
         strip.begin();
         strip.show();
@@ -16,15 +24,11 @@ void setup() {
 
 
 void loop() {
-        int grid[HEIGHT][WIDTH] = {
-                {1,0,1,0,1,0,0,0},
-                {1,0,1,0,0,0,0,0},
-                {1,1,1,0,1,0,0,0},
-                {1,0,1,0,1,0,0,0},
-        };
-
         printGrid(grid);
+        shiftGrid(grid);
+        delay(500);
 }
+
 
 void printGrid(int grid[HEIGHT][WIDTH]) {
         int cell = 0;
@@ -32,10 +36,21 @@ void printGrid(int grid[HEIGHT][WIDTH]) {
                 for (int j = 0; j < WIDTH; j++) {
                         if (grid[i][j] == 1) {
                                 strip.setPixelColor(cell, strip.Color(255,0,0));
+                        } else {
+                                strip.setPixelColor(cell, strip.Color(0,0,0));
                         }
                         cell++;
                 }
         }
 
         strip.show();
+}
+
+
+void shiftGrid(int grid[HEIGHT][WIDTH]) {
+        for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
+                        grid[i][j] = grid[i][(j+1)%(WIDTH-1)];
+                }
+        }
 }
